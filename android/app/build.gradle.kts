5442fb9a0ef3ab0431db00f2833a76d6b1beff76
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-06-22 17:08:39 +0000

    Fix compileSdk 36, vibration v3, APK builds successfully

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.votrite.votrite_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
